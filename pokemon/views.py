2261d41d378bbae4463bc7ef3620a0a09c6d39d6
ALAN KARLO MANGUBAT <karlomangubat92@gmail.com>
2021-03-20 15:12:50 -0700

    added permissions

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,11 +1,16 @@
+from pokemon.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
-from .serializer import PokemonSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Pokemon
+from .serializer import PokemonSerializer
 
-class PokemonList(generics.ListAPIView):
+class PokemonList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
 
-class PokemonDetail(generics.RetrieveAPIView):
+class PokemonDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
+
--- pokemon_api/urls.py
+++ pokemon_api/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/pokemon/', include('pokemon.urls'))
+    path('api/v1/pokemon/', include('pokemon.urls')),
+    path("api-auth/", include("rest_framework.urls")),
 ]
